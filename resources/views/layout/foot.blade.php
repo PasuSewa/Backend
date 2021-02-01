954ee5b835d13c0a64cc5e9a0bb4092cce06e4dd
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-02-01 09:55:26 -0300

    added the layout with creative-tim/argon-design

---
+++ resources/views/layout/foot.blade.php
@@ -0,0 +1,27 @@
+<!--   Core JS Files   -->
+<script src="{{asset('argon')}}/js/core/jquery.min.js" type="text/javascript"></script>
+<script src="{{asset('argon')}}/js/core/popper.min.js" type="text/javascript"></script>
+<script src="{{asset('argon')}}/js/core/bootstrap.min.js" type="text/javascript"></script>
+<script src="{{asset('argon')}}/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+<!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
+<script src="{{asset('argon')}}/js/plugins/bootstrap-switch.js"></script>
+<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+<script src="{{asset('argon')}}/js/plugins/nouislider.min.js" type="text/javascript"></script>
+<!--  Plugin for the Carousel, full documentation here: http://jedrzejchalubek.com/ -->
+<script src="{{asset('argon')}}/js/plugins/glide.js" type="text/javascript"></script>
+<!--  Plugin for the DatePicker, full documentation here: https://flatpickr.js.org/ -->
+<script src="{{asset('argon')}}/js/plugins/moment.min.js"></script>
+<!--	Plugin for Select, full documentation here: https://joshuajohnson.co.uk/Choices/ -->
+<script src="{{asset('argon')}}/js/plugins/choices.min.js" type="text/javascript"></script>
+<!--  Plugin for the DateTimePicker, full documentation here: https://flatpickr.js.org/ -->
+<script src="{{asset('argon')}}/js/plugins/datetimepicker.js" type="text/javascript"></script>
+<!-- Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
+<script src="{{asset('argon')}}/js/plugins/jasny-bootstrap.min.js"></script>
+<!-- Plugin for Headrom, full documentation here: https://wicky.nillia.ms/headroom.js/ -->
+<script src="{{asset('argon')}}/js/plugins/headroom.min.js"></script>
+<!-- Control Center for Argon UI Kit: parallax effects, scripts for the example pages etc -->
+<!-- Place this tag in your head or just before your close body tag. -->
+<script src="{{asset('argon')}}/js/argon-design-system.min.js?v=1.0.0" type="text/javascript"></script>
+</body>
+
+</html>
---
+++ resources/views/layout/head.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="{{asset('argon')}}/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="{{asset('argon')}}/img/favicon.png">
+  <title>
+    PasuSewa Dashboard
+  </title>
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+  <!-- Nucleo Icons -->
+  <link href="{{asset('argon')}}/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="{{asset('argon')}}/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <link href="{{asset('argon')}}/css/font-awesome.css" rel="stylesheet" />
+  <link href="{{asset('argon')}}/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link href="{{asset('argon')}}/css/argon-design-system.css?v=1.0.1" rel="stylesheet" />
+</head>
+
+<body class="index-page">
+  
+    
