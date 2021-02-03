ff7ce58eaab1fc1c69b7fbe43c7beca439cbd718
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-02-03 18:51:53 -0300

    changed the name for the logo url column

--- database/migrations/2021_02_03_142544_create_companies_table.php
+++ database/migrations/2021_02_03_142544_create_companies_table.php
@@ -16,7 +16,7 @@ class CreateCompaniesTable extends Migration
         Schema::create('companies', function (Blueprint $table) {
             $table->increments('id');
             $table->string('company_name');
-            $table->string('url_company_logo')->nullable();
+            $table->string('url_logo')->nullable();
             $table->timestamps();
         });
     }
