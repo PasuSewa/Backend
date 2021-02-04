c4affc240fe9988672aba723e0c599433f810c86
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-02-04 19:21:29 -0300

    added the border radius for all cards, and the proto for statistics card

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,5 +1,21 @@
 @include('layout.head')
 <body class="{{ $class ?? '' }}">
+    <style>
+        .card{
+            border-radius: 10px !important;
+        }
+        .card-body{
+            border-radius: 10px !important;
+        }
+        .card-header{
+            border-top-left-radius: 10px !important;
+            border-top-right-radius: 10px !important;
+        }
+        .card-footer{
+            border-bottom-left-radius: 10px !important;
+            border-bottom-right-radius: 10px !important;
+        }
+    </style>
     <main>
         @yield('content')
     </main>
