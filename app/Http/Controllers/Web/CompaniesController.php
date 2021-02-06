a4cc9f581c63e2c2a8bf042e0eda3408aa1c057b
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-02-06 09:47:49 -0300

    created companies controller

---
+++ app/Http/Controllers/Web/CompaniesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CompaniesController extends Controller
+{
+    public function index()
+    {
+        // 
+    }
+
+    public function create()
+    {
+        // 
+    }
+
+    public function update()
+    {
+        //
+    }
+
+    public function delete()
+    {
+        //
+    }
+}
