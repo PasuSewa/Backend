43bff4f51480322868ae009810c720d1a3135c5e
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-06-15 11:41:04 -0300

    prepeared everything for uploading to heroku

--- config/app.php
+++ config/app.php
@@ -52,9 +52,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://pasunashi-backend.herokuapp.com/'),
 
-    'asset_url' => env('ASSET_URL', null),
+    'asset_url' => env('ASSET_URL', 'https://pasunashi-backend.herokuapp.com/'),
 
     /*
     |--------------------------------------------------------------------------
