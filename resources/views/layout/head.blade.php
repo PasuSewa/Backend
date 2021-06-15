aa748cad1ad9111836456fa84af8124f2c1a7d6a
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-06-15 11:47:59 -0300

    (☞ﾟヮﾟ)☞ ☜(ﾟヮﾟ☜)

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="apple-touch-icon" sizes="76x76" href="{{asset('argon')}}/img/apple-icon.png">
   <title>
-    PasuSewa Dashboard
+    PasuNashi Dashboard
   </title>
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
