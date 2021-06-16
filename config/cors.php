02e08b5f3003030370ad9a38d012fdb855d8ecbb
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-06-16 18:06:51 -0300

    CORS SOLUTIONgit add .

--- config/cors.php
+++ config/cors.php
@@ -15,6 +15,25 @@ return [
     |
     */
 
+    /**
+     * La solución para el cors es la siguiente:
+     * 
+     * 1) Aprendible tiene un video en el que lo explica, buscar por CORS
+     * 
+     * 2) Aprendible muestra que habria que poner el dominio exacto del que esperamos las peticiones, ya que a mi 
+     * no me funcionó, decidí dejarlo como un * y resolvió el problema
+     * 
+     * En caso de no encontrar el video de Aprendible.com, 
+     * 
+     * 1) instalar el paquete fruitcacke/cors
+     * 
+     * 2) publicar el vendor
+     * 
+     * 3) en config/cors.php dejar todo como acá
+     * 
+     * 4) agregar el middleware publicado a la lista de middlewares en kernel (el array de $middlewares)
+     */
+
     'paths' => ['api/*'],
 
     'allowed_methods' => ['*'],
