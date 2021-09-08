7742b688c74269b06a90c468ba5ea067f0db26bb
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2021-09-08 14:50:56 -0300

    added more routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,25 +13,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::group(['middleware' => 'Localization'], function () {
+
     Route::post('/send-code-by-email', 'AuthController@sendCodeByEmail');
 
     Route::get('/feedback/index', 'FeedbackController@index');
 
+    Route::group(['middleware' => 'guest:api', 'prefix' => 'auth'], function () {
 
-    Route::group(['middleware' => 'guest:api'], function () {
+        //step 3 requires the user to be authenticated
         Route::post('/register/step-1', 'AuthController@create_user');
 
         Route::post('/register/step-2', 'AuthController@verify_emails');
-
-        Route::post('/register/step-3', 'AuthController@verify_2fa');
     });
 
     Route::group(['middleware' => 'auth:api'], function () {
+
+        Route::group(['prefix' => 'auth'], function () {
+            Route::post('/register/step-3', 'AuthController@verify_2fa');
+
+            Route::get('/refresh-2fa-secret', 'AuthController@refresh_2fa_secret');
+        });
+
         Route::post('/feedback/create', 'FeedbackController@create')->middleware('role:premium');
     });
 });
