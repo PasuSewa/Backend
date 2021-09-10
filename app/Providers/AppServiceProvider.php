ca74d79363bfad5d5f2a7f8383cf5dbfdbd78875
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2021-09-10 15:41:57 -0300

    small changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -36,32 +36,47 @@ class AppServiceProvider extends ServiceProvider
             ], 200);
         });
 
-        Response::macro('user_was_authenticated', function ($data, $message, $with_token = false, $with_credentials = false) {
+        Response::macro(
+            'user_was_authenticated',
+            function (
+                $data,
+                $message,
+                $with_token = false,
+                $with_credentials = false,
+                $mobile = false
+            ) {
 
-            if ($with_credentials) {
-                $user_credentials = [];
-            } else {
-                $user_credentials = [];
-            }
+                if ($with_credentials) {
+                    $user_credentials = [];
+                } else {
+                    $user_credentials = [];
+                }
 
-            return response()->json([
-                'status' => 200,
-                'message' => __('api_messages.success.auth.' . $message),
-                'data' => [
-                    'user_data' => [
-                        'id' => $data['user']->id,
-                        'name' => $data['user']->name,
-                        'email' => $data['user']->email,
-                        'recovery_email' => $data['user']->recovery_email,
-                        'slots_available' => $data['user']->slots_available,
-                        'invitation_code' => $data['user']->invitation_code,
-                        'role' => $data['user']->getRoleNames()[0], // users only have 1 role
-                        'token' => $with_token ? auth('api')->setTTL(7200)->tokenById($data['user']->id) : null,
+                return response()->json([
+                    'status' => 200,
+                    'message' => __('api_messages.success.auth.' . $message),
+                    'data' => [
+                        'user_data' => [
+                            'id' => $data['user']->id,
+                            'name' => $data['user']->name,
+                            'email' => $data['user']->email,
+                            'recovery_email' => $data['user']->recovery_email,
+                            'slots_available' => $data['user']->slots_available,
+                            'invitation_code' => $data['user']->invitation_code,
+                            'role' => $data['user']->getRoleNames()[0], // users only have 1 role
+                            'token' => $with_token
+                                ?
+                                auth('api')
+                                ->setTTL($mobile ? null : 60) // 60 mins if plattform is different from mobile
+                                ->tokenById($data['user']->id)
+                                :
+                                null,
+                        ],
+                        'user_credentials' => $user_credentials
                     ],
-                    'user_credentials' => $user_credentials
-                ],
-            ], 200);
-        });
+                ], 200);
+            }
+        );
 
         Response::macro('user_was_authorized', function ($message) {
             return response()->json([
