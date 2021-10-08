63d8c2b83cf6c5a8be32136947a77f51653dadef
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2021-10-08 14:30:18 -0300

    installed laravel-request-docs

---
+++ config/request-docs.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'document_name'  => 'PasuNashi InternalDocs',
+
+    /*
+    * Route where request docs will be served from
+    * localhost:8080/request-docs
+    */
+    'url' => 'request-docs',
+    'middlewares' => [
+        //Example
+        \App\Http\Middleware\Localization::class,
+    ],
+    /**
+     * Path to to static HTML if using command line.
+     */
+    'docs_path' => base_path('docs/request-docs/'),
+
+    /**
+     * Sorting route by and there is two types default(route methods), route_names.
+     */
+    'sort_by' => 'default',
+
+    'hide_matching' => [
+        "#^telescope#",
+        "#^docs#",
+        "#^request-docs#",
+    ]
+];
